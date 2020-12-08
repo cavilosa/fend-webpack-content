df51310d6fad3d8775cb51d8f40a1a3c109ee7d2
Maryna Korzhyk <cavilosa33@gmail.com>
2020-12-08 13:40:13 -0700

    Analizer and clean plugins

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,6 @@ import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
 console.log(checkForName);
+console.log("This is webpack dev server");
 
 alert("I EXIST")
