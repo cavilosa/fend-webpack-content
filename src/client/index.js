6e3501b1e4fea9da98cb379bf4bb9c717f26409e
Maryna Korzhyk <cavilosa33@gmail.com>
2020-12-08 10:07:29 -0700

    File loader for footer pic

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,13 @@
-import { checkForName } from "./js/nameChecker"
-import { handleSubmit } from "./js/formHandler"
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+import img from "./images/footer.jpg";
+
+
+/*const image = new Image();
+image.src = img;
+
+const footer = document.querySelector("footer");
+footer.appendChild(image);*/
 
 console.log(checkForName)
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: "./src/client/index.js",
     module: {
-                rules: [
-                        {
-                    test: '/\.js$/',
-                    exclude: /node_modules/,
-                    loader: "babel-loader"
-                        }
-                ]
-        }
-}
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                    },
+                ],
+            },
+        ],
+    },
+};
